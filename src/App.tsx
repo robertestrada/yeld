ecbbcded5fa6ebd86b2aa454408f3f85ac5a5a88
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2021-07-25 14:15:31 -0700

    Add Not Found page for 404 errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LandingPage from './components/Landing/LandingPage';
 import Results from './components/Results/ResultsPage';
 import BusinessPage from './components/Business/BusinessPage';
+import NotFoundPage from './components/NotFoundPage';
 import './App.css';
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/biz" render={() => <BusinessPage />} />
-        <Route path="/search" render={() => <Results />} />
+        <Route path="/biz/:alias" render={ () => <BusinessPage /> } />
+        <Route path="/search" render={ () => <Results /> } />
         <Route path="/" exact render={ () => <LandingPage /> } />
+        <Route render={() => <NotFoundPage />} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (<>Not Found Page (404 Error)</>)
+}
+
+export default NotFoundPage;
