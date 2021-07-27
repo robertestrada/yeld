3103feeccc252f93b8b367037d427b4a14c4c7db
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2021-07-27 16:56:10 -0700

    Successfully connect client fetch with server fetch to yelp and returned correct response

---
+++ client/src/components/utilities/yelpAPI.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import queryString from 'query-string';
+import { YELP_API_BASE_URL, YELP_BEARER_TOKEN, baseUrl } from '../utilities/config';
+
+export const getAutoSuggestions = async (params: string) => {
+  const response = await fetch(`${baseUrl}/testAPI${params}`);
+  if (response.ok){
+    const result = await response.json();
+    console.log("suggestions: ", result);
+  }
+}
--- src/components/utilities/config.tsx
+++
@@ -1,2 +0,0 @@
-export const YELP_BEARER_TOKEN = process.env.REACT_APP_YELP_API_KEY;
-export const YELP_API_BASE_URL = 'https://api.yelp.com/v3';
