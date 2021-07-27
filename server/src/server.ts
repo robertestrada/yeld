d3151f1759bb8eba504f1df4c2dd8a87d809c2c5
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2021-07-27 10:46:51 -0700

    Attempt to fix express not serving react build, blank screen problem

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
 import path from 'path';
-const app = express()
+// import cors from 'cors';
+
 const port: string | number = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/yeld-api', (req, res) => {
 
   // Return them as json
@@ -14,7 +20,7 @@ app.get('/yeld-api', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 
 app.listen(port);
