51e486c4fb6d75757e1f8b75f4eafce666bc63b5
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2021-07-27 19:39:54 -0700

    Refactor server yelpApi with successful client connection and add try catch2

--- client/src/components/utilities/yelpAPI.tsx
+++ client/src/components/utilities/yelpAPI.tsx
@@ -3,7 +3,7 @@ import queryString from 'query-string';
 import { YELP_API_BASE_URL, YELP_BEARER_TOKEN, baseUrl } from '../utilities/config';
 
 export const getAutoSuggestions = async (params: string) => {
-  const response = await fetch(`${baseUrl}/testAPI${params}`);
+  const response = await fetch(`${baseUrl}/yelpAPI${params}`);
   if (response.ok){
     const result = await response.json();
     console.log("suggestions: ", result);
