49af954ed0b87d93bee3dc3431bb480704b7a0d3
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2021-07-29 01:18:03 -0700

    Add navbar logo

--- client/src/components/NavBar/Navbar.tsx
+++ client/src/components/NavBar/Navbar.tsx
@@ -1,14 +1,23 @@
 import NavSearchBar from './NavBarComponents/NavSearchBar';
 import { Dispatch, SetStateAction } from "react";
+import '../../styles/NavBar.css';
+import yeldData from '../../data/yeldData';
+
 
 const NavBar = (props: { location: string, setLocation: Dispatch<SetStateAction<string>>, term: string, setTerm: Dispatch<SetStateAction<string>>, userLocation: string }) => {
+  const logoUrl = yeldData.landingPage.logoUrl;
   
-  console.log(props);
   
   return (
     <div className="NavBar">
-      NavBar
-      <NavSearchBar {...props} />
+      <div className="NavBar__header">
+        {/* <div className="NavBar__logo-container"> */}
+          <h1 className="NavBar__logo" style={{ backgroundImage: `url(${logoUrl})` }}>
+            <a href="/">Yeld</a>
+          </h1>
+        {/* </div> */}
+        <NavSearchBar {...props} />
+      </div>
     </div>)
 }
 
